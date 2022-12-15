5998bd690b167428807e89049bd11ef7adfbd7aa
Pramod <prqamodraina047@gmail.com>
2022-12-15 15:04:07 +0000

    Updated Not Found Component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,7 +2,11 @@ import './index.css'
 
 const NotFound = () => (
   <div className="not-found-container">
-    <h1 className="not-found-heading">Page Not Found!!</h1>
+    <img
+      src="https://res.cloudinary.com/dlpgowt5s/image/upload/v1671116371/404_Error-rafiki_1_i5lnw2.png"
+      alt="not found"
+      className="not-found-image"
+    />
   </div>
 )
 
